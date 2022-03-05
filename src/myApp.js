40ae4c80d613ed4a8be4be0fd9143256e7d70584
Michael Obubelebra Amachree <70345027+Michael-Obele@users.noreply.github.com>
2022-03-05 08:57:32 +0100

    First Delete stuff

--- src/myApp.js
+++ src/myApp.js
@@ -3,15 +3,23 @@ import React, { Component } from 'react';
 export default class Time extends Component {
   constructor(props) {
     super(props);
-    this.setState = {
+    this.state = {
       counter: 0,
     };
+    this.Click = this.Click.bind(this);
+  }
+
+  Click() {
+    this.setState((state, prevState) => ({
+      counter: state.counter + 1,
+    }));
   }
   render() {
     return (
       <div>
         <h1>Header</h1>
-        <h1>Counter:{this.state.counter}</h1>
+        <h2>Counter:{this.state.counter}</h2>
+        <button onClick={this.Click}>Click to Add</button>
       </div>
     );
   }
