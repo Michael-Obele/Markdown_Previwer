4f7ed1619c988c56ef1b8f18f001b291b8e4dc4c
Michael Obubelebra Amachree <70345027+Michael-Obele@users.noreply.github.com>
2022-03-05 08:21:12 +0100

    Start

---
+++ src/myApp.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Time extends Component {
+  constructor(props) {
+    super(props);
+    this.setState = {
+      counter: 0,
+    };
+  }
+  render() {
+    return <div></div>;
+  }
+}
