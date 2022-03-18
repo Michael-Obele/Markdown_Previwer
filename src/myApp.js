29fd499e37a47d302d33d53f980ef8d1f029e2cc
Michael Obubelebra Amachree <70345027+Michael-Obele@users.noreply.github.com>
2022-03-18 12:52:17 +0100

    Added font awesome icons

--- src/myApp.js
+++ src/myApp.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import * as actions from './app/store';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 
 export const App = (props) => {
   const count = useSelector((state) => state.counter);
@@ -40,6 +42,9 @@ export const App = (props) => {
       </div>
     );
   };
+  const Moon = <FontAwesomeIcon icon={faMoon} />;
+  const Sun = <FontAwesomeIcon icon={faSun} />;
+
   const color = (x) => {
     return DarkMode ? `Dark${x}` : x;
   };
@@ -48,12 +53,12 @@ export const App = (props) => {
       <h1>Header</h1>
       {DarkMode ? (
         <div>
-          <button onClick={ButtonAction.LightMode}>Light Mode</button>
+          <button onClick={ButtonAction.LightMode}>Light Mode {Sun}</button>
           <h4>DarkMode On</h4>
         </div>
       ) : (
         <div>
-          <button onClick={ButtonAction.DarkMode}>Dark Mode</button>
+          <button onClick={ButtonAction.DarkMode}>Dark Mode {Moon}</button>
           <h4>DarkMode Off</h4>
         </div>
       )}
