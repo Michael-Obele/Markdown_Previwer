c5e9d3fefa538ccc90d6cf305b095985074e456d
Michael Obubelebra Amachree <70345027+Michael-Obele@users.noreply.github.com>
2022-03-20 20:27:28 +0100

    so far am tired. I'll continue later

--- src/content.js
+++ src/content.js
@@ -1,22 +1,48 @@
 import React, { useState } from 'react';
 import * as p from 'react-bootstrap';
-import Markdown from 'marked-react';
+import { useSelector } from 'react-redux';
+import { marked } from 'marked';
+// or const { marked } = require('marked');
 
 export const Content = () => {
+  const DarkMode = useSelector((state) => state.DarkMode);
+  const dark = () => {
+    return {
+      bg: () => {
+        return DarkMode ? 'dark' : 'light';
+      },
+      text: () => {
+        return DarkMode ? 'light' : 'dark';
+      },
+      color: () => {
+        return DarkMode ? 'white' : 'black';
+      },
+      backgroundColor: () => {
+        return DarkMode ? '#212529' : 'white';
+      },
+    };
+  };
   const [markdown, setMarkdown] = useState('# Hello world!');
   const updateMarkdown = (markdown) => {
     setMarkdown(markdown);
   };
+  const html = marked.parse(markdown);
+  function createMarkup() {
+    return { __html: html };
+  }
+  function MyComponent() {
+    return <div dangerouslySetInnerHTML={createMarkup()} />;
+  }
   return (
     <div>
-      <p.Row id='input'>
+      <p.Row id='input' className='justify-content-center'>
         <p.Form.Control
           as='textarea'
           style={{
-            height: '20vh',
+            height: '30vh',
             width: '50vw',
-            backgroundColor: '#212529',
-            color: 'white',
+            backgroundColor: dark().backgroundColor(),
+            color: dark().color(),
           }}
           onChange={(e) => {
             updateMarkdown(e.target.value);
@@ -27,14 +53,14 @@ export const Content = () => {
         <p.Card
           id='output'
           border='dark'
-          text='light'
-          bg='dark'
+          text={dark().text()}
+          bg={dark().bg()}
           style={{ height: 'fit-content', width: '50vw' }}
         >
           <p.Card.Header>Header</p.Card.Header>
           <p.Card.Body>
             <p.Card.Text>
-              <Markdown>{markdown}</Markdown>
+              <MyComponent />
             </p.Card.Text>
           </p.Card.Body>
         </p.Card>
