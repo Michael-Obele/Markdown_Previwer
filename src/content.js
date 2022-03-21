26e01caf3ecab42a11d23d4fc27aa87d73ba172b
Michael Obubelebra Amachree <70345027+Michael-Obele@users.noreply.github.com>
2022-03-21 14:21:13 +0100

    Current state

--- src/content.js
+++ src/content.js
@@ -6,25 +6,41 @@ import { marked } from 'marked';
 
 export const Content = () => {
   const dummyText = `
-  # Heading level 1
+   ## Hi <img src="https://c.tenor.com/Wx9IEmZZXSoAAAAj/hi.gif" height="25" width="25" >
 
-  ## Heading level 2
+# I’m Michael Obele
 
-  ### A Link
-  This is my [Website](https://moaconcept.xyz).
+#### I'm a \`<Web/>\` **Developer**
 
-  ### A code
-  At the command prompt, type ${'`nano`'}
+\`\`\`
+function Developer(you, I) {
+    const collaborate = () => {
+      const I = 'Michael',
+        am = 'is';
+      if (you) {
+        return \`\${I} \${am} now collaborating with \${you}\`;
+      } else {
+        return \`\${I} \${am} free to collaborate\`;
+      }
+    };
+    return collaborate();
+  }
+  \`\`\`
 
-  ### A code block
-   <html>
-      <head>
-      <h1>Hey</h1>
-      <h2>You</h2>
-      </head>
-    </html>
+- 👀 I’m searching for organizations and or persons to collaborate on open-source projects!
+
+- 🌱 I’m currently learning  <img src="https://github.com/Michael-Obele/Gif/blob/main/icons8-javascript.gif?raw=true" align="center" height="28" width="28"> <img src="https://github.com/Michael-Obele/Gif/blob/main/icons8-react.gif?raw=true" align="center" height="28" width="28"> and <img src="https://github.com/Michael-Obele/Gif/blob/main/icons8-python.gif?raw=true" align="center" height="28" width="28">
+
+- 📫 You can reach me on Twitter [@Dev_Obele](https://twitter.com/Dev_Obele) or check out my [website](https://moaconcept.xyz)
+
+<h3 align="center">I Love coding ❤️</h3>
+<p align="center">
+   <a href="https://github.com/Michael-Obele">
+   <img src="https://c.tenor.com/NOYF3f82b_gAAAAC/programmer.gif" align="center" height="97" width="134" ></a>
+</p>
 
 # Something more
+  
   `;
   const DarkMode = useSelector((state) => state.DarkMode);
   const dark = () => {
