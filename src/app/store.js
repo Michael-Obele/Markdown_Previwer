4bb6fb7d355d82b6269ae99afc9395099bbdd1d2
Michael Obubelebra Amachree <70345027+Michael-Obele@users.noreply.github.com>
2022-04-02 12:03:41 +0100

    .

--- src/app/store.js
+++ src/app/store.js
@@ -31,6 +31,7 @@ const Reducers = combineReducers({
 
 const store = createStore(
   Reducers,
+  persistedState,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 // store.subscribe(() => console.log(store.getState()));
