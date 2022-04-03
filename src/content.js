df72529db4b9b44b0c68afd2f86e30140cd664b3
Michael Obubelebra Amachree <70345027+Michael-Obele@users.noreply.github.com>
2022-04-03 05:47:03 +0100

    text

--- src/content.js
+++ src/content.js
@@ -1,47 +1,12 @@
 import React, { useState } from 'react';
 import * as p from 'react-bootstrap';
 import { useSelector } from 'react-redux';
+import { Text } from './dummyText';
 import { marked } from 'marked';
 // or const { marked } = require('marked');
 
 export const Content = () => {
-  const dummyText = `
-   ## Hi <img src="https://c.tenor.com/Wx9IEmZZXSoAAAAj/hi.gif" height="25" width="25" >
-
-# I’m Michael Obele
-
-#### I'm a \`<Web/>\` **Developer**
-
-\`\`\`
-function Developer(you, I) {
-    const collaborate = () => {
-      const I = 'Michael',
-        am = 'is';
-      if (you) {
-        return \`\${I} \${am} now collaborating with \${you}\`;
-      } else {
-        return \`\${I} \${am} free to collaborate\`;
-      }
-    };
-    return collaborate();
-  }
-  \`\`\`
-
-- 👀 I’m searching for organizations and or persons to collaborate on open-source projects!
-
-- 🌱 I’m currently learning  <img src="https://github.com/Michael-Obele/Gif/blob/main/icons8-javascript.gif?raw=true" align="center" height="28" width="28"> <img src="https://github.com/Michael-Obele/Gif/blob/main/icons8-react.gif?raw=true" align="center" height="28" width="28"> and <img src="https://github.com/Michael-Obele/Gif/blob/main/icons8-python.gif?raw=true" align="center" height="28" width="28">
-
-- 📫 You can reach me on Twitter [@Dev_Obele](https://twitter.com/Dev_Obele) or check out my [website](https://moaconcept.xyz)
-
-<h3 align="center">I Love coding ❤️</h3>
-<p align="center">
-   <a href="https://github.com/Michael-Obele">
-   <img src="https://c.tenor.com/NOYF3f82b_gAAAAC/programmer.gif" align="center" height="97" width="134" ></a>
-</p>
-
-# Something more
-  
-  `;
+  <Text />;
   const DarkMode = useSelector((state) => state.DarkMode);
   const dark = () => {
     return {
@@ -59,7 +24,7 @@ function Developer(you, I) {
       },
     };
   };
-  const [markdown, setMarkdown] = useState(dummyText);
+  const [markdown, setMarkdown] = useState(Text);
   const updateMarkdown = (markdown) => {
     setMarkdown(markdown);
   };
