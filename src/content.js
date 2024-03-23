36a6cd660307be8bf2728805b93a0e892a7f4e5b
Michael Obubelebra Amachree <amachree9630@gmail.com>
2024-03-23 20:58:12 +0100

    Refactor content.js for cleaner code and efficiency: Simplify dark mode style functions, streamline text formatting functions, and remove unnecessary useEffect.

--- src/content.js
+++ src/content.js
@@ -9,25 +9,16 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 import file from './dummyText.md';
 import Expand from './Modal';
-// import Markdown from 'react-markdown';
 import { marked } from 'marked';
 
 export const Content = () => {
   const DarkMode = useSelector((state) => state.DarkMode);
   const dark = () => {
     return {
-      bg: () => {
-        return DarkMode ? 'dark' : 'light';
-      },
-      text: () => {
-        return DarkMode ? 'light' : 'dark';
-      },
-      color: () => {
-        return DarkMode ? 'white' : 'black';
-      },
-      backgroundColor: () => {
-        return DarkMode ? '#212529' : 'white';
-      },
+      bg: DarkMode ? 'dark' : 'light',
+      text: DarkMode ? 'light' : 'dark',
+      color: DarkMode ? 'white' : 'black',
+      backgroundColor: DarkMode ? '#212529' : 'white',
     };
   };
 
@@ -41,33 +32,16 @@ export const Content = () => {
   };
 
   const bold = () => {
-    const startIndex = markdown.indexOf(value);
-    const selectedText = markdown.substring(
-      startIndex,
-      startIndex + value.length
-    );
-    setMarkdown(markdown.replace(selectedText, `**${selectedText}**`));
+    setMarkdown((prevMarkdown) => prevMarkdown.replace(value, `**${value}**`));
   };
 
   const italic = () => {
-    const startIndex = markdown.indexOf(value);
-    const selectedText = markdown.substring(
-      startIndex,
-      startIndex + value.length
-    );
-    setMarkdown(markdown.replace(selectedText, `*${selectedText}*`));
+    setMarkdown((prevMarkdown) => prevMarkdown.replace(value, `*${value}*`));
   };
 
   const quote = () => {
-    const startIndex = markdown.indexOf(value);
-    const selectedText = markdown.substring(
-      startIndex,
-      startIndex + value.length
-    );
-    setMarkdown(markdown.replace(selectedText, `\n > ${selectedText}`));
+    setMarkdown((prevMarkdown) => prevMarkdown.replace(value, `\n > ${value}`));
   };
-
-  // Remove unnecessary useEffect
 
   useEffect(() => {
     fetch(file)
@@ -82,18 +56,19 @@ export const Content = () => {
   function createMarkup() {
     return { __html: marked.parse(markdown).replace(/(<\/h(1|2)>)/gm, '<hr>') };
   }
+
   function TheMarkup() {
     return <div dangerouslySetInnerHTML={createMarkup()} />;
   }
 
   return (
-    <div>
+    <>
       <p.Row id='input' className='justify-content-center'>
         <p.Card
           id='output'
           border='dark'
-          text={dark().text()}
-          bg={dark().bg()}
+          text={dark().text}
+          bg={dark().bg}
           style={{ height: 'fit-content', width: '85vw' }}>
           <p.Card.Header>
             <div className='row  justify-content-start'>
@@ -109,28 +84,27 @@ export const Content = () => {
               </p.Button>
             </div>
             <Expand Title='Editor'>
-              {' '}
               <p.Form.Control
                 as='textarea'
                 style={{
                   height: '70vh',
-                  backgroundColor: dark().backgroundColor(),
-                  color: dark().color(),
+                  backgroundColor: dark().backgroundColor,
+                  color: dark().color,
                 }}
                 value={markdown}
                 onChange={(e) => {
                   updateMarkdown(e.target.value);
                 }}
               />
-            </Expand>{' '}
+            </Expand>
           </p.Card.Header>
           <p.Form.Control
             onMouseUp={handleMouseUp}
             as='textarea'
             style={{
               height: '30vh',
-              backgroundColor: dark().backgroundColor(),
-              color: dark().color(),
+              backgroundColor: dark().backgroundColor,
+              color: dark().color,
             }}
             value={markdown}
             onChange={(e) => {
@@ -141,15 +115,14 @@ export const Content = () => {
         <p.Card
           id='output'
           border='dark'
-          text={dark().text()}
-          bg={dark().bg()}
+          text={dark().text}
+          bg={dark().bg}
           style={{ height: 'fit-content', width: '85vw' }}>
           <p.Card.Header>
-            <div className='p-2 fs-5 col-sm-1'>Preview</div>{' '}
+            <div className='p-2 fs-5 col-sm-1'>Preview</div>
             <Expand Title='Preview'>
-              {' '}
               <TheMarkup />
-            </Expand>{' '}
+            </Expand>
           </p.Card.Header>
           <p.Card.Body>
             <p.Card.Text>
@@ -158,6 +131,6 @@ export const Content = () => {
           </p.Card.Body>
         </p.Card>
       </p.Row>
-    </div>
+    </>
   );
 };
